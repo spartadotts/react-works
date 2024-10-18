f15797e147cb1c79e8f5835b803b883430ae1395
spartadotts <keshav.shri06@gmail.com>
2024-10-18 13:52:38 +0530

    add: QR code generator

--- src/components/QRcode/Qrcode.tsx
+++ src/components/QRcode/Qrcode.tsx
@@ -1,27 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
 import QRCode from "react-qr-code";
 
-export default function Qrcode(){
+export default function Qrcode() {
+  const [input, setInput] = useState<string>("");
+  const [qrcode, setQrcode] = useState<string>("");
 
-    const [input,setInput] = useState<string>('');
-    const [qrcode, setQrcode] = useState<string>('');
+  function handleGenerate() {
+    setQrcode(input);
+  }
 
-    function handleGenerate(){
-        setQrcode(input);
-
-    }
-
-    return <div style={{
-        textAlign: "center"
-    }}>
-        <h1>QR Code Generator</h1>
-        <input placeholder="Enter text to generate QR" title="QR data" onChange={e => setInput(e.target.value)} ></input>
-        <button onClick={handleGenerate}>Generate QR</button>
-        <div style={{
-            margin: "20px"
-        }}>
-            <QRCode value={qrcode} size={300}/>
-        </div>
+  return (
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1>QR Code Generator</h1>
+      <input
+        placeholder="Enter text to generate QR"
+        title="QR data"
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <button onClick={handleGenerate}>Generate QR</button>
+      <div
+        style={{
+          margin: "20px",
+        }}
+      >
+        <QRCode value={qrcode} size={300} />
+      </div>
     </div>
-
+  );
 }
